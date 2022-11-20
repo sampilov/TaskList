fc949434fef79616d37e079dd67804c090eaca9d
Ildar Sampilov <ildar.sampilov@yandex.ru>
2022-11-20 15:38:57 +0600

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by Цырендылыкова Эржена on 20.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
